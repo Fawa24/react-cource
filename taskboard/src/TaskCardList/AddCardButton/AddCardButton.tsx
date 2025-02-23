a4825581542fb6bbb6a094525dd09ef73d03eb32
Fawa24 <ofeglol@gmail.com>
2025-02-23 19:23:45 +0200

    Improved styles for AddCardButton

--- taskboard/src/TaskCardList/AddCardButton/AddCardButton.tsx
+++ taskboard/src/TaskCardList/AddCardButton/AddCardButton.tsx
@@ -3,6 +3,7 @@ import "./AddCardButton.css";
 
 export default function AddCardButton() {
   return <Card className="task-card-list-component add-card-button">
-    Add new card
+    <i className="bi bi-plus fs-5"></i>
+    <p className="m-0">Add new card</p>
   </Card>
 }
