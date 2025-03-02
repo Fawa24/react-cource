728e07e5f6eeb221847592c3e7b40f8446d3fb0d
Fawa24 <ofeglol@gmail.com>
2025-03-02 13:03:48 +0200

    Added problem (createUser function executes on every component re-rendering) to solve with useMemo

--- reactHooks/useMemo-demo/src/App.tsx
+++ reactHooks/useMemo-demo/src/App.tsx
@@ -1,10 +1,26 @@
-function App() {
+import { useState } from "react"
 
+function createUser(name: string, surname: string) {
+  const user = { name, surname };
+  console.log(user);
+  return user;
+}
+
+function App() {
+  const [name, setName] = useState<string>('');
+  const [surname, setSurname] = useState<string>('');
+  const [counter, setCounter] = useState<number>(0);
+
+  const user = createUser(name, surname);
+  
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <button onClick={() => setCounter(counter + 1)}>Button was clicked {counter} times</button>
+      <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
+      <input type='text' value={surname} onChange={(e) =>setSurname(e.target.value)} />
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+    </div>
+  );
 }
 
 export default App
