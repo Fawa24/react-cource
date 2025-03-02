104990e746cb38f272a4d2965658bfc6a457f454
Fawa24 <ofeglol@gmail.com>
2025-03-02 12:41:53 +0200

    Fixed problem with useId hook

--- reactHooks/useId-demo/src/LabelInput.tsx
+++ reactHooks/useId-demo/src/LabelInput.tsx
@@ -1,6 +1,11 @@
+import { useId } from "react"
+
 export function LabelInput() {
+  const generatedId = useId();
+  const inputId = `${generatedId}-input`;
+
   return <form style={{marginBottom: '50px', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-    <label htmlFor="input" style={{marginBottom: '7px', fontSize: '20px'}}>Label for input</label>
-    <input id="input"></input>
+    <label htmlFor={inputId} style={{marginBottom: '7px', fontSize: '20px'}}>Label for input</label>
+    <input id={inputId}></input>
   </form>
 }
