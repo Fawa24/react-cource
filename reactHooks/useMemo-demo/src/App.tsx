ea81bd7ba134a23f29fea987171b9fbb295ed5c3
Fawa24 <ofeglol@gmail.com>
2025-03-02 13:43:29 +0200

    Small improvement

--- reactHooks/useMemo-demo/src/App.tsx
+++ reactHooks/useMemo-demo/src/App.tsx
@@ -6,19 +6,25 @@ function createUser(name: string, surname: string) {
   return user;
 }
 
+interface User {
+  name: string;
+  surname: string;
+}
+
 function App() {
   const [name, setName] = useState<string>('');
   const [surname, setSurname] = useState<string>('');
   const [counter, setCounter] = useState<number>(0);
 
-  const user = createUser(name, surname);
+  const user: User = createUser(name, surname);
   
   return (
     <div>
       <button onClick={() => setCounter(counter + 1)}>Button was clicked {counter} times</button>
       <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
       <input type='text' value={surname} onChange={(e) =>setSurname(e.target.value)} />
-      <pre>{JSON.stringify(user, null, 2)}</pre>
+      <pre>name:    {user.name} </pre>
+      <pre>surname: {user.surname}</pre>
     </div>
   );
 }
