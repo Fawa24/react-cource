81b77879fe90b3a51e7ee211dcb1d113f9648aee
Fawa24 <ofeglol@gmail.com>
2025-03-03 21:23:48 +0200

    Added simple useReducer hook usage result

--- reactHooks/useReducer-demo/src/App.tsx
+++ reactHooks/useReducer-demo/src/App.tsx
@@ -1,8 +1,48 @@
+import { useReducer } from "react";
+
+const buttonStyle = {
+  width: '40px', 
+  fontSize: '20px'
+};
+
+enum ActionType {
+  Increment = 0,
+  Decrement = 1
+}
+
+interface CountStateType {
+  count: number;
+}
+
+function reducer(state: CountStateType, actionType: ActionType) {
+  switch (actionType) {
+    case ActionType.Increment:
+      return { count: state.count + 1 };
+    case ActionType.Decrement:
+      return { count: state.count - 1};
+    default:
+      return state;
+  };
+  
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+  function increnemt() {
+    dispatch(ActionType.Increment);
+  }
+
+  function decrement() {
+    dispatch(ActionType.Decrement);
+  }
+
   return (
-    <>
-      
-    </>
+    <div style={{display: 'flex', width: '100px', justifyContent: 'space-between'}}>
+      <button onClick={increnemt} style={buttonStyle}>+</button>
+      <p>{state.count}</p>
+      <button onClick={decrement} style={buttonStyle}>-</button> 
+    </div>
   )
 }
 
