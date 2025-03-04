6f2ca23a976b5c0fa84ab7e1bc80a3ddc8f78482
Fawa24 <ofeglol@gmail.com>
2025-03-04 22:54:17 +0200

    Implemented controller

--- ReactHookForm/HookFormsWithBackend/HookFormsWithBackend/Controllers/CardController.cs
+++ ReactHookForm/HookFormsWithBackend/HookFormsWithBackend/Controllers/CardController.cs
@@ -2,9 +2,22 @@
 
 namespace HookFormsWithBackend.Controllers
 {
+    public class Card
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
     [Route("/")]
     public class CardController : Controller
     {
+        [HttpPost]
+        public ActionResult SaveCard([FromBody] Card card)
+        {
+            var name = card.Name;
+            var description = card.Description;
 
+            return Ok();
+        }
     }
 }
