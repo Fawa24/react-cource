96ab172696a4b3988a4a0b329b4341579ffb24c0
Fawa24 <ofeglol@gmail.com>
2025-03-04 23:22:22 +0200

    Added form

--- ReactHookForm/HookFormsWithBackend/Client/src/App.tsx
+++ ReactHookForm/HookFormsWithBackend/Client/src/App.tsx
@@ -1,8 +1,34 @@
+import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+
+const style = { display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center' };
+
+interface Card {
+  name: string;
+  description: string;
+}
+
 function App() {
+  const { register, handleSubmit } = useForm<Card>();
+
+  function onSubmitClick(card: Card) {
+    
+  }
+
   return (
-    <>
-      
-    </>
+    <div style={style}>
+      <Form style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '18rem' }}>
+        <Form.Group className="mb-3">
+          <Form.Label>Card name</Form.Label>
+          <Form.Control type="text" placeholder="New Card" {...register('name')} />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Card description</Form.Label>
+          <Form.Control type="text" placeholder="Card description" {...register('description')} />
+        </Form.Group>
+        <Button variant="primary" onClick={handleSubmit(onSubmitClick)}>Submit</Button>
+      </Form>
+    </div>
   )
 }
 
