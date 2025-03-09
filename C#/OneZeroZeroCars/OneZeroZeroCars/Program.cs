326f87e9ce72412c0cbe6ec261fd7cebf1084b1b
Fawa24 <ofeglol@gmail.com>
2025-03-09 16:41:44 +0200

    Added problem: timers running synhronosly

--- C#/OneZeroZeroCars/OneZeroZeroCars/Program.cs
+++ C#/OneZeroZeroCars/OneZeroZeroCars/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
+            var mainThr = Thread.CurrentThread;
+            mainThr.Name = "Main thread";
 
+            CountDown();
+            CountUp();
+
+            Console.WriteLine($"{mainThr.Name} is completed");
+
+            Console.ReadKey();
+        }
+
+        public static void CountDown()
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine($"Timer #1: {i} seconds left");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void CountUp()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"Timer #2: {i} seconds counted");
+                Thread.Sleep(1000);
+            }
         }
     }
 }
