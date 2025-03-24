53b056a2110c06ca4eafcb4a892be934383f8a69
Fawa24 <ofeglol@gmail.com>
2025-03-24 10:47:44 +0200

    Added printGuessStatistic problem

---
+++ CSharp/Clean Code/printGuessStatisticProblem/printGuessStatisticProblem/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace printGuessStatisticProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintGuessStatistic("candidate", 0);
+        }
+
+        private static void PrintGuessStatistic(string candidate, int count)
+        {
+            string number;
+            string verb;
+            string pluralModifier;
+            if (count == 0)
+            {
+                number = "no";
+                verb = "is";
+                pluralModifier = "s";
+            }
+            else if (count == 1)
+            {
+                number = "1";
+                verb = "is";
+                pluralModifier = "";
+            }
+            else
+            {
+                number = count.ToString();
+                verb = "are";
+                pluralModifier = "s";
+            }
+
+            Console.WriteLine($"There {verb} {number} {candidate}{pluralModifier}");
+        }
+    }
+}
